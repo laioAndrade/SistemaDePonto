13971f2f22c71105fe1b9b2c2071956a1174c596
laioAndrade <laioand98@gmail.com>
2018-10-30 15:51:02 -0300

    first commit

---
+++ connection/config.php
@@ -0,0 +1,9 @@
+<?php
+	/* Aqui você informa as informações do seu banco de dados */
+	define('DB_HOSTNAME', 'localhost');
+	define('DB_USERNAME', 'mateus');
+	define('DB_PASSWORD', 'Mateus@123');
+	define('DB_DATABASE', 'biometria');
+	define('DB_PREFIX', '');
+	define('DB_CHARSET', 'utf8');
+?>
